9e20ebd5e331e3e7e406a9fc68496919fc1ddfcb
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-27 13:04:09 +0530

    pointer assignment & solution

--- 07-pointers/program.go
+++ 07-pointers/program.go
@@ -17,10 +17,10 @@ func main() {
 	n1 := 10
 	n2 := 20
 	fmt.Printf("Before swapping n1 = %d and n2 = %d\n", n1, n2)
-	swap( /* ... */ )
+	swap(&n1, &n2)
 	fmt.Printf("After swapping n1 = %d and n2 = %d\n", n1, n2)
 }
 
-func swap() {
-
+func swap(n1Ptr *int, n2Ptr *int) {
+	*n1Ptr, *n2Ptr = *n2Ptr, *n1Ptr
 }
