f5bf806e9dc63dead30911dd84faea50d3caf507
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-27 10:32:00 +0530

    slices demo

--- 04-functions/variadicFn.go
+++ 04-functions/variadicFn.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println(add(10, 20))
 	fmt.Println(add(10, 20, 30))
 	fmt.Println(add(10, 20, 30, 40, 50))
+
+	var nosSlice []int = []int{9, 4, 2, 6, 1}
+	fmt.Println(add(nosSlice...))
 }
 
 func add(nos ...int) int {
--- 05-collections/program.go
+++ 05-collections/program.go
@@ -13,7 +13,7 @@ func main() {
 
 	//initilization while creation
 	//var products [5]string = [5]string{"Pen", "Pencil", "Marker", "Scribble Pad", "Mouse"}
-	var products = []string{"Pen", "Pencil", "Marker", "Scribble Pad", "Mouse"}
+	var products = [5]string{"Pen", "Pencil", "Marker", "Scribble Pad", "Mouse"}
 	fmt.Printf("Type of products = %T\n", products)
 	fmt.Printf("No of products = %v\n", len(products))
 
@@ -30,4 +30,31 @@ func main() {
 		}
 	}
 	fmt.Printf("Matrix - %v\n", matrix)
+
+	//Slices
+	var nosSlice []int = []int{9, 4, 2, 6, 1}
+	fmt.Println("nosSlice = ", nosSlice)
+
+	//adding new items to the slice using the 'append' function
+	nosSlice = append(nosSlice, 5, 14, 12)
+	fmt.Println("After appending, nosSlice = ", nosSlice)
+
+	anotherNosSlice := []int{100, 200, 300}
+	nosSlice = append(nosSlice, anotherNosSlice...)
+	fmt.Println("After appending, nosSlice = ", nosSlice)
+
+	//slicing a Slice
+	/*
+		nosSlice[lo:hi] => [0:4] = [0, 1, 2, 3]
+		nosSlice[lo:lo] => empty
+		nosSlice[lo:lo+1] => item at 'lo'
+		nosSlice[lo:] => all the values from 'lo'
+		nosSlice[:hi] => all the values from 0 to 'hi-1'
+	*/
+	fmt.Printf("nosSlice[1:4] = %v\n", nosSlice[1:4])
+	fmt.Printf("nosSlice[1:1] = %v\n", nosSlice[1:1])
+	fmt.Printf("nosSlice[1:2] = %v\n", nosSlice[1:2])
+	fmt.Printf("nosSlice[5:] = %v\n", nosSlice[5:])
+	fmt.Printf("nosSlice[:5] = %v\n", nosSlice[:5])
+
 }
