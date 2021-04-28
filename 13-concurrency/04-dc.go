5149d457ec0c7d64a47a730eec64d447122c417c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-28 17:00:54 +0530

    updates

---
+++ 13-concurrency/04-dc.go
@@ -0,0 +1,14 @@
+package main
+
+func sum( /* list */ ) {
+
+}
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+	/*
+		1. break the data into 2 equal sets
+		2. run 2 goroutine instances of sum function one for each set
+		3. get the result from each of them and aggregate and print the final result
+	*/
+}
