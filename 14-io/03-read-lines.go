1569c5a284b68e573ce27a21e04b0a64a82a73e0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-29 14:18:29 +0530

    reading files line by lines (or sentences)

---
+++ 14-io/03-read-lines.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	fileHandle, fileError := os.Open("sample.txt")
+	if fileError != nil {
+		log.Fatalln(fileError)
+	}
+	inputReader := bufio.NewReader(fileHandle)
+	for {
+		line, err := inputReader.ReadString('.')
+		fmt.Println(line)
+		if err == io.EOF {
+			break
+		}
+	}
+	fileHandle.Close()
+}
